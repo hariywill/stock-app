b5f840e29c39c8dee2369f455431ef2f0a6cc3a1
Chris <n10612823@qut.edu.au>
2021-05-25 15:17:14 +1000

    Basic views

--- App.js
+++ App.js
@@ -2,10 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Main from './app/views/main/Main'
+import Header from './app/views/header/Header'
+import Footer from './app/views/footer/Footer'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
       <StatusBar style="auto" />
     </View>
   );
---
+++ app/views/main/Main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Main = () => {
+    return (
+        <p>this is main</p>
+    )
+}
+
+export default Main;
